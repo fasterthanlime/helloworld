9a55dea80d3fff07fbc271c98f65ce4800e42518
Amos Wenger <amos@official.fm>
2012-02-14 21:50:00 +0100

    Version initiale qui affiche Hello World

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloworld::Application.config.secret_token = 'fec46eb31569681d955856daf2ace7a01959b277afa35386b2789826daf1ea34f3d58129f5583b7ad35974d1016b68124d29870aef87618cfd8c07f0d7a849e0'
